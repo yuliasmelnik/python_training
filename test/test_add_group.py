1a3bad950d8ee29d194bf981517542e63aac6909
Melnik <jusapeshkina@gmail.com>
2025-06-27 22:56:43 +0300

    Проверка текущего состояния сессии

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,6 +5,5 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name ="adfds", header ="ffggh", footer ="gdhre"))
 
-
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
