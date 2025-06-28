5ab46a0cc6a89c6418ac5271b828cb6052ba4416
Melnik <jusapeshkina@gmail.com>
2025-06-29 00:01:48 +0300

    Задание 10

--- fixture/contact.py
+++ fixture/contact.py
@@ -91,7 +91,8 @@ class ContactHalper:
 
     def return_to_contacts_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
