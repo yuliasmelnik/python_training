45afa773a2a3c497db8c5e8b80d7786f10a12907
Melnik <jusapeshkina@gmail.com>
2025-06-28 12:06:33 +0300

    Проверка, что есть хотя бы один контакт для удаления

--- fixture/contact.py
+++ fixture/contact.py
@@ -92,3 +92,8 @@ class ContactHalper:
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
 
+    def count(self):
+        wd = self.app.wd
+        self.open_home_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from model.contact import Contact
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.delete_first_contact()
