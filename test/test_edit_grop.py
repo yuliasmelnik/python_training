9c5d13b50e782a164d10b5fce1e8342433a30ede
Melnik <jusapeshkina@gmail.com>
2025-07-05 00:13:49 +0300

    Сравнение списков групп в тесте на добавление и модификации групп

--- test/test_edit_grop.py
+++ test/test_edit_grop.py
@@ -4,8 +4,11 @@ from model.group import Group
 
 def test_edit_first_group(app):
     if app.group.count() == 0:
-        app.group.create(Group(name="test", header ="test", footer ="test"))
-    old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name ="editn", header ="edith", footer ="editf"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+        old_groups = app.group.get_group_list()
+        group = Group(name ="editn", header ="edith", footer ="editf")
+        group.id = old_groups[0].id
+        app.group.edit_first_group(group)
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) == len(new_groups)
+        old_groups[0] = group
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
