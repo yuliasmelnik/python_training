f25298e800649eed2dfa2b0a0aa33e5e0ec4de58
Melnik <jusapeshkina@gmail.com>
2025-07-04 23:26:41 +0300

    Список групп

--- test/test_edit_grop.py
+++ test/test_edit_grop.py
@@ -5,4 +5,7 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test", header ="test", footer ="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name ="editn", header ="edith", footer ="editf"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
