5574e1fcefd9a0039000b01d41a131bf67b10c27
Melnik <jusapeshkina@gmail.com>
2025-07-07 16:54:17 +0300

    Удаление случайной группы

--- fixture/group.py
+++ fixture/group.py
@@ -38,10 +38,17 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         #submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
