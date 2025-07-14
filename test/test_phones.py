e9632e304bcde37afbcd4cbc17a32e72141e186d
Melnik <jusapeshkina@gmail.com>
2025-07-14 22:57:35 +0300

    Задание 15

--- test/test_phones.py
+++ test/test_phones.py
@@ -7,9 +7,11 @@ def test_phones_on_home_page(app):
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.all_phones_from_view_page == merge_phones_like_on_view_page(contact_from_edit_page)
+    if (contact_from_edit_page.mobilephone != "" and contact_from_edit_page.workphone != "" and
+            contact_from_edit_page.homephone != ""):
+        contact_from_view_page = app.contact.get_contact_from_view_page(0)
+        assert contact_from_view_page.all_phones_from_view_page == merge_phones_like_on_view_page(contact_from_edit_page)
 
 def clear(s):
     return re.sub("[() -]", "", s)
