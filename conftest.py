9b591ea9adcfa726179b2808d76a5e4c686cefa7
Melnik <jusapeshkina@gmail.com>
2025-07-19 15:23:43 +0300

    Конфигурационный файл

--- conftest.py
+++ conftest.py
@@ -1,20 +1,21 @@
 import pytest
+import json
 from fixture.application import Application
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    password = request.config.getoption("--password")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url, password=password)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url, password=password)
-    fixture.session.ensure_login(username="admin", password=password)
+    if target is None:
+        with open (request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -28,6 +29,5 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--password", action="store", default="secret")
+    parser.addoption("--target", action="store", default="target.json")
 
--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ from fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self, browser, base_url, password):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -20,7 +20,6 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHalper(self)
         self.base_url = base_url
-        self.password=password
 
     def is_valid(self):
         try:
