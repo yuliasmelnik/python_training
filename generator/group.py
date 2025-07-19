a607b52683892dd8496055748b3987082abee432
Melnik <jusapeshkina@gmail.com>
2025-07-19 21:53:47 +0300

    Загрузка тестовых данных из файлов в формате JSON

--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,7 @@
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 from model.group import Group
@@ -16,7 +16,7 @@ except getopt.GetoptError as err:
     sys.exit(2)
 
 n = 5
-f = "data/group.json"
+f = "data/groups.json"
 
 for o, a in opts:
     if o == "-n":
@@ -36,4 +36,5 @@ testdata = [Group(name="", header="", footer="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
