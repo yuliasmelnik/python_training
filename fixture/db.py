7f0ef92b6f1566f06abcac846f05be7c3632bae3
Melnik <jusapeshkina@gmail.com>
2025-07-25 12:01:39 +0300

    Ограничения или условия отбора загружаемых данных в тесте по удалению контакта

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -11,6 +12,18 @@ class DbFixture:
         self.password = password
         self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
 
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated is null")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+        finally:
+            cursor.close()
+        return list
+
     def get_group_list(self):
         list = []
         cursor = self.connection.cursor()
@@ -21,7 +34,7 @@ class DbFixture:
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
         finally:
             cursor.close()
-            return list
+        return list
 
     def destroy(self):
         self.connection.close()
