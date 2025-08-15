7598435c0da1fade8c55b9c4ca7e6d01f0c61940
Melnik <jusapeshkina@gmail.com>
2025-08-15 21:29:17 +0300

    BDD

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -1,12 +1,12 @@
-from pytest_bdd import given, when, then
+from pytest_bdd import given, when, then, parsers
 from model.group import Group
 import random
 
-@given('a group list')
+@given('a group list', target_fixture='group_list')
 def group_list(db):
     return db.get_group_list()
 
-@given('a group with <name>, <header> and <footer>')
+@given(parsers.parse('a group with {name}, {header} and {footer}'), target_fixture='new_group')
 def new_group(name, header, footer):
     return Group(name=name, header=header, footer=footer)
 
@@ -21,19 +21,19 @@ def verify_group_added(db, group_list, new_group):
     old_groups.append(new_group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-@given('a non-empty group list')
+@given('a non-empty group list', target_fixture='non_empty_group_list')
 def non_empty_group_list(db, app):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name='some name'))
     return db.get_group_list()
 
-@given('a random group from the list')
+@given('a random group from the list', target_fixture='random_group')
 def random_group(non_empty_group_list):
     return random.choice(non_empty_group_list)
 
 @when('I delete the group from the list')
 def delete_group(app, random_group):
-    app.group.delete_group_by_id(random_group)
+    app.group.delete_group_by_id(random_group.id)
 
 @then('the new group list is equal to the old list without the deleted group')
 def verify_group_deleted(db, non_empty_group_list, random_group, app, check_ui):
