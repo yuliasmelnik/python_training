bb155445b61a249cc8f21125c4ff3840eb1614d5
Melnik <jusapeshkina@gmail.com>
2025-08-17 15:25:33 +0300

    Robot Framework

---
+++ rf/group_scenarios.robot
@@ -0,0 +1,24 @@
+*** Settings ***
+Library  rf.AddressBook
+Library  Collections
+Suite Setup  Init Fixtures
+Suite Teardown  Destroy Fixtures
+
+*** Test Cases ***
+Add_new_group
+    ${old_list}=  Get Group List
+    ${group}=  New Group  name1  header1  footer1
+    Create Group  ${group}
+    ${new_list}=  Get Group List
+    Append To List  ${old_list}  ${group}
+    Group Lists Should Be Equal  ${new_list}  ${old_list}
+
+Delete group
+    ${old_list}=  Get Group List
+    ${len}=  Get Length  ${old_list}
+    ${index}=  Evaluate  random.randrange(${len})  random
+    ${group}=  Get From List  ${old_list}  ${index}
+    Delete Group  ${group}
+    ${new_list}=  Get Group List
+    Remove Values From List  ${old_list}  ${group}
+    Group Lists Should Be Equal  ${new_list}  ${old_list}
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
+import allure
 from model.group import Group
 
 
 def test_add_group(app, db, json_groups, check_ui):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with allure.step('When I add a group %s to the list' %group):
+        app.group.create(group)
+    with allure.step('the new group list is equal to the old list with the added group %s' % group):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
